9cf4c3adf453d5f299148fa3a45209c6ddb8cb2c
Marcelo Czerewacz <marcelo@akun.ai>
2020-11-26 11:16:47 +0100

    Fix Missing Entity Primary Key

--- app/src/main/java/com/raywenderlich/android/librarian/model/Book.kt
+++ app/src/main/java/com/raywenderlich/android/librarian/model/Book.kt
@@ -37,12 +37,14 @@ package com.raywenderlich.android.librarian.model
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
 @Entity(tableName = "books")
 class Book(
+    @PrimaryKey
     val id: String = UUID.randomUUID().toString(),
     val name: String,
     val description: String,
